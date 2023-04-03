18d9dfb74541b26a9082da5466a484180261f868
Tom Shinton <tomshinton@googlemail.com>
2023-04-03 22:43:58 +0100

    Added watermarking to Server, User and Workspace fields, and obscured any password text from showing in real text

--- UPT_UI/Source/Windows/PerforceConnectWindow.xaml.cs
+++ UPT_UI/Source/Windows/PerforceConnectWindow.xaml.cs
@@ -15,10 +15,58 @@ namespace UPT_UI.Windows
         }
 
         private void ConnectButton_OnClick(object sender, RoutedEventArgs e)
-        { 
+        {
             MainWindow.ReportProgress("Attempting to initialise P4 connection");
-            MainWindow.SourceControlHandler.Connect(ServerURLBox.Text, UserNameBox.Text, WorkspaceNameBox.Text, PasswordBox.Text);
+            MainWindow.SourceControlHandler.Connect(ServerURLBox.Text, UserNameBox.Text, WorkspaceNameBox.Text, PasswordBox.Password);
             this.Close();
+        }
+
+        private void ServerURLBoxWatermark_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            ServerURLBoxWatermark.Visibility = Visibility.Collapsed;
+            ServerURLBox.Visibility = Visibility.Visible;
+            ServerURLBox.Focus();
+        }
+
+        private void ServerURLBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(ServerURLBox.Text))
+            {
+                ServerURLBox.Visibility = Visibility.Collapsed;
+                ServerURLBoxWatermark.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UserNameBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(UserNameBox.Text))
+            {
+                UserNameBox.Visibility = Visibility.Collapsed;
+                UserNameBoxWatermark.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UserNameBoxWatermark_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            UserNameBoxWatermark.Visibility = Visibility.Collapsed;
+            UserNameBox.Visibility = Visibility.Visible;
+            UserNameBox.Focus();
+        }
+
+        private void WorkspaceNameBox_OnLostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(WorkspaceNameBox.Text))
+            {
+                WorkspaceNameBox.Visibility = Visibility.Collapsed;
+                WorkspaceNameBoxWatermark.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void WorkspaceNameBoxWatermark_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            WorkspaceNameBoxWatermark.Visibility = Visibility.Collapsed;
+            WorkspaceNameBox.Visibility = Visibility.Visible;
+            WorkspaceNameBox.Focus();
         }
     }
 }
